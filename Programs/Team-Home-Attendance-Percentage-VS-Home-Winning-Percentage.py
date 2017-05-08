6e303cf199909c2c87342f7d4cbddffdaa9e5a52
KeithCissell <keithacissell@gmail.com>
2017-05-07 19:27:09 -0500

    Created new files that analyze these same 2 stats per team instead of overall. Heavily updated the readme

--- Programs/Team-Home-Attendance-Percentage-VS-Home-Winning-Percentage.py
+++ Programs/Team-Home-Attendance-Percentage-VS-Home-Winning-Percentage.py
@@ -16,7 +16,7 @@ get_ipython().magic('matplotlib inline')
 teams_file = "Data//Teams.csv"
 ballparks_file = "Data//Ballparks.csv"
 
-gl_location = "Data//Retrosheet//GL"
+path = "Data//Retrosheet//GL"
 startyr = 1970
 endyr = 2016
 
@@ -45,13 +45,30 @@ def readcsv(fname):
     return lines
 
 
-# In[40]:
+# In[21]:
 
 class Team:
-    def __init__(self, team, teamid, yr, cap):
-        self.team = team
+    def __init__(self, team_name, teamid):
+        self.name = team_name
         self.teamid = teamid
-        self.year = yr
+        self.years = {}
+    def addGame(self, game, year, cap):
+        if year not in self.years:
+            self.years[year] = YearData(year, cap)
+        self.years[year].addGame(game)
+    def setStats(self):
+        for yr in self.years:
+            year = self.years[yr]
+            year.setAvgAtt()
+            year.setAttPct()
+            year.setWinPct()
+
+
+# In[22]:
+
+class YearData:
+    def __init__(self, year, cap):
+        self.year = year
         self.park_capacity = cap
         self.avg_att = 0
         self.att_pct = 0
@@ -85,7 +102,7 @@ class Team:
         self.win_pct = wins / num_games
 
 
-# In[41]:
+# In[23]:
 
 def build_teamid_ref(lines):
     d = {}
@@ -96,7 +113,7 @@ def build_teamid_ref(lines):
     return d
 
 
-# In[42]:
+# In[24]:
 
 def build_park_ref(lines):
     d = {}
@@ -108,98 +125,85 @@ def build_park_ref(lines):
     return d
 
 
-# In[43]:
+# In[25]:
 
-# builds a dictionary -> {year: {team_name: Team, ...}, ...}
-def build_years_dict():
+# builds a dictionary -> {teamid: Team, ...}
+def build_teams_dict():
     # create the teamid dictionary for refference
     team_lines = readcsv(teams_file)
     park_lines = readcsv(ballparks_file)
     team_ref = build_teamid_ref(team_lines[1:])
     parks_ref = build_park_ref(park_lines[1:])
     
-    years = {}
+    teams = {}
     for yr in range(startyr, (endyr + 1)):
-        # add year to dict and initialize a dict for teams
-        years[yr] = {}
-        # gather game data for year
-        yr_file = gl_location + str(yr) + ".csv"
+        yr_file = path + str(yr) + ".csv"
         games = readcsv(yr_file)
         for game in games:
             # add valid data
             park = game[parkx]
             if park and park in parks_ref:
-                if game[attx]:                
-                    if int(game[attx]) > 0:
-                        # check if team is in team dict or not
-                        teamid = game[teamx]
-                        team_name = team_ref[teamid]
-                        if team_name not in years[yr]:
-                            cap = parks_ref[park]
-                            years[yr][team_name] = Team(team_name, teamid, yr, cap)
-                        # add game data to proper team
-                        years[yr][team_name].addGame(game)
+                cap = parks_ref[park]
+                if game[attx] and int(game[attx]) > 0:
+                    # check if team is in team dict or not
+                    teamid = game[teamx]
+                    team_name = team_ref[teamid]
+                    if teamid not in teams:
+                        teams[teamid] = Team(team_name, teamid)
+                    # add game data to proper team
+                    teams[teamid].addGame(game, yr, cap)
 
     # set attendance and win percentages
-    for yr in years:
-        for team in years[yr]:
-            t = years[yr][team]
-            t.setAvgAtt()
-            t.setAttPct()
-            t.setWinPct()
+    for team in teams:
+        teams[team].setStats()
     
-    return years
+    return teams
 
 
-# In[44]:
+# In[37]:
 
-def attendancePct_vs_winningPct(years):
+def attendancePct_vs_winningPct(team):
     attendancePct = []
     winningPct = []
-    for y in years:
-        year = years[y]
-        for t in year:
-            team = year[t]
-            attendancePct.append(team.att_pct)
-            winningPct.append(team.win_pct)
+    for y in team.years:
+        year = team.years[y]
+        attendancePct.append(year.att_pct)
+        winningPct.append(year.win_pct)
     # plot the data
+    yrs = team.years.keys()
+    minYr = str(min(yrs))
+    maxYr = str(max(yrs))
     xlabel = "Attendance Percentage"
     ylabel = "Winning Percentage"
-    plot_data(attendancePct, winningPct, xlabel, ylabel)
+    title = team.name + "(" + minYr + "-" + maxYr + "): Winning Pct. VS Attendance Pct."
+    plot_data(attendancePct, winningPct, xlabel, ylabel, title)
 
 
-# In[51]:
+# In[41]:
 
-def plot_data(X, Y, xlabel, ylabel):
+def plot_data(X, Y, xlabel, ylabel, title):
     fig = plt.figure()
     ax = plt.axes()
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    title = ylabel + " VS. " + xlabel
     ax.set_title(title)
     ax.plot(X, Y, '.')
     
     corr = pearsonr(X, Y)
-    print("Correlation:", round(corr[0] * 100, 2))
+    print(title, "Correlation:", round(corr[0] * 100, 2))
 
 
-# In[52]:
+# In[42]:
 
-# team_lines = readcsv(teams_file)
-# park_lines = readcsv(ballparks_file)
-# team_ref = build_teamid_ref(team_lines[1:])
-# parks_ref = build_park_ref(park_lines[1:])
-# print(parks_ref)
+teams = build_teams_dict()
 
 
-# In[53]:
+# In[45]:
 
-years = build_years_dict()
-
-
-# In[54]:
-
-attendancePct_vs_winningPct(years)
+sortedTeams = sorted(teams.keys())
+for tid in sortedTeams:
+    team = teams[tid]
+    attendancePct_vs_winningPct(team)
 
 
 # In[ ]:
